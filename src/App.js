d4e47aef35e16cf1f3b12b4c66f95cf2d2903e41
ogzhanolguncu <ogzhan11@gmail.com>
2022-01-08 00:42:38 +0300

    Init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import { winningConditions } from "./utils";
+
+//TODO: RESET GAME
+//TODO: USER CANNOT CLICK THE SAME SPOT
+//TODO: OPTIMIZE currentPosition reduce for the winning condition
+//TODO: IMPROVE DESIGN
+//TODO: ADD ANIMATION
+//TODO: SHIP THIS TO VERCEL
+//TODO: ADD MINIMAX ALGO
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [board, setBoard] = useState(new Array(9).fill(null));
+	const [isX, setIsX] = useState(true);
+	const [isGameOver, setIsGameOver] = useState(undefined);
+
+	const findWinnerPlayer = (currentPosition) => {
+		const winner = winningConditions.reduce(
+			(acc, crr) => {
+				if (crr.every((item) => currentPosition.X.includes(item))) acc.X = true;
+				if (crr.every((item) => currentPosition.Y.includes(item))) acc.Y = true;
+				return acc;
+			},
+			{ X: false, Y: false }
+		);
+
+		if (winner.X || winner.Y) setIsGameOver(winner.X ? "X" : "Y");
+	};
+
+	const handleTileClick = (index) => {
+		const copiedBoard = [...board];
+		copiedBoard[index] = isX ? "X" : "Y";
+
+		const currentPosition = copiedBoard.reduce(
+			(acc, crr, index) => {
+				if (crr === "X") acc.X.push(index);
+				if (crr === "Y") acc.Y.push(index);
+				return acc;
+			},
+			{ X: [], Y: [] }
+		);
+
+		if (copiedBoard.filter(Boolean).length >= 5) {
+			findWinnerPlayer(currentPosition);
+		}
+
+		setBoard(copiedBoard);
+		setIsX((prevState) => !prevState);
+	};
+	if (isGameOver) {
+		return (
+			<div
+				className="App"
+				style={{ display: "flex", height: "100vh", justifyContent: "center", alignItems: "center" }}
+			>
+				Gameover - Winner is {isGameOver}
+			</div>
+		);
+	}
+
+	return (
+		<div
+			className="App"
+			style={{ display: "flex", height: "100vh", justifyContent: "center", alignItems: "center" }}
+		>
+			<div className="grid-container">
+				{board.map((item, index) => (
+					<div className="grid-item" key={index} onClick={() => handleTileClick(index)}>
+						{item}
+					</div>
+				))}
+			</div>
+		</div>
+	);
 }
 
 export default App;
