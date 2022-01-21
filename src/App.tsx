bd35fcc79aeae503323b14ee70072c0c94fab4e8
Oguzhan Olguncu <oguzhan@fireflyon.com>
2022-01-22 00:23:37 +0300

    Add ts

--- src/App.tsx
+++ src/App.tsx
@@ -10,52 +10,55 @@ import { winningConditions } from "./utils";
 //TODO: SHIP THIS TO VERCEL
 //TODO: ADD MINIMAX ALGO
 
+type CurrentPosition = { X: number[]; O: number[] };
+type GameOverStates = "X" | "O" | "Draw";
+
 function App() {
   const [board, setBoard] = useState(new Array(9).fill(null));
   const [isX, setIsX] = useState(true);
   const [reset, setReset] = useState(false);
-  const [isGameOver, setIsGameOver] = useState(undefined);
+  const [isGameOver, setIsGameOver] = useState<GameOverStates>();
+
+  //UNION TYPE
 
   const cols = Array.from(document.querySelectorAll(".grid-item"));
-  const colorizeSelectedBoxes = (winner, ...args) =>
-    args[0][winner].forEach((item) => cols[item].classList.add("win"));
+  const colorizeSelectedBoxes = (winner: "X" | "O", ...args: any[]) =>
+    args[0][winner].forEach((item: number) => cols[item].classList.add("win"));
 
-  const findWinnerPlayer = (currentPosition) => {
+  const findWinnerPlayer = (currentPosition: CurrentPosition) => {
     const winner = winningConditions.reduce(
       (acc, crr) => {
         if (crr.every((item) => currentPosition.X.includes(item))) {
           acc.X = true;
-          acc.XMoves = currentPosition;
+          acc.XMoves = currentPosition.X;
         }
         if (crr.every((item) => currentPosition.O.includes(item))) {
           acc.O = true;
-          acc.OMoves = currentPosition;
+          acc.OMoves = currentPosition.O;
         }
         return acc;
       },
-      { X: false, O: false }
+      { X: false, XMoves: [] as number[], O: false, OMoves: [] as number[] }
     );
 
     const isWinnerX = winner.X ? "X" : "O";
-    console.log(isWinnerX, winner);
     if (winner.X || winner.O) {
       colorizeSelectedBoxes(isWinnerX, currentPosition);
       setIsGameOver(isWinnerX);
     }
-    console.log({
-      board,
-      draw: !winner.X && !winner.O && board.length === 9,
-    });
     if (!winner.X && !winner.O && board.filter(Boolean).length === 9) {
-      setIsGameOver("berabere");
+      setIsGameOver("Draw");
     }
   };
 
   if (isGameOver) {
-    document.querySelector(".tabel").style.backgroundColor = "red";
+    if (document.querySelector<HTMLElement>(".tabel")) {
+      document.querySelector<HTMLElement>(".tabel")!.style.backgroundColor =
+        "red";
+    }
   }
 
-  const handleTileClick = (index) => {
+  const handleTileClick = (index: number) => {
     if (reset || isGameOver || board[index]) return;
     const copiedBoard = [...board];
     copiedBoard[index] = isX ? "X" : "O";
@@ -82,42 +85,41 @@ function App() {
     setIsX(true);
     setBoard(Array(9).fill(null));
     setReset(false);
-    document.querySelector(".tabel").style.backgroundColor = "#76AFD6";
+    document.querySelector<HTMLElement>(".tabel")!.style.backgroundColor =
+      "#76AFD6";
   };
 
   return (
-    <div>
-      <body class="w-screen h-screen ctr">
-        <div class="tabel ctr">
-          <div class="board">
-            {board.map((item, index) => (
-              <div
-                className="grid-item"
-                key={index}
-                onClick={() => handleTileClick(index)}
-              >
-                {item}
-              </div>
-            ))}
-          </div>
-          <button id="reset" onClick={() => handleReset()}>
-            Reset
-          </button>
-          {isGameOver && (
+    <div className="w-screen h-screen ctr">
+      <div className="tabel ctr">
+        <div className="board">
+          {board.map((item, index) => (
             <div
-              className="App"
-              style={{
-                display: "flex",
-                height: "100vh",
-                justifyContent: "center",
-                alignItems: "center",
-              }}
+              className="grid-item"
+              key={index}
+              onClick={() => handleTileClick(index)}
             >
-              Winner is {isGameOver}
+              {item}
             </div>
-          )}
+          ))}
         </div>
-      </body>
+        <button id="reset" onClick={() => handleReset()}>
+          Reset
+        </button>
+        {isGameOver && (
+          <div
+            className="App"
+            style={{
+              display: "flex",
+              height: "100vh",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            Winner is {isGameOver}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
